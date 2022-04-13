5e853d53cae1ae823ad07d1cfb03df1f2a6177f8
Natalia Teixeira <nataliat1026@gmail.com>
2022-04-13 09:38:40 -0400

    commit tree b626c6c3634f77828895db122165da36bc2712ce

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const drivers = [];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = function() {
+    return [
+        returnFirstTwoDrivers(drivers),
+        returnLastTwoDrivers(drivers)   
+    ];
+}
+
+const createFareMultiplier = function(i) {
+
+    return function(fare) {
+        (fare * i);
+    }
+}
+
